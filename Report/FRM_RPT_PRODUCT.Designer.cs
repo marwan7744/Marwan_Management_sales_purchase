0ebabb3d7fa73f46034a630d5715b4d23ccf13aa
marwan7744 <marwan71814@gmail.com>
2025-08-04 02:35:42 +0300

    Add files via upload

---
+++ Report/FRM_RPT_PRODUCT.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace MANGEMENT.Report
+{
+    partial class FRM_RPT_PRODUCT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Location = new System.Drawing.Point(5, 12);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ProcessingMode = Microsoft.Reporting.WinForms.ProcessingMode.Remote;
+            this.reportViewer1.Size = new System.Drawing.Size(759, 694);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // FRM_RPT_PRODUCT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 729);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "FRM_RPT_PRODUCT";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "تقرير المنتوجات";
+            this.Load += new System.EventHandler(this.FRM_RPT_PRODUCT_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
---
+++ Report/FRM_RPT_PRODUCT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MANGEMENT.Report
+{
+    public partial class FRM_RPT_PRODUCT : Form
+    {
+        public FRM_RPT_PRODUCT()
+        {
+            InitializeComponent();
+        }
+
+        private void FRM_RPT_PRODUCT_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
